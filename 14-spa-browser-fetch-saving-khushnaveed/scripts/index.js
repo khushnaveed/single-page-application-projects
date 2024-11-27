3737c9b0ec1ffa69a4312066e0236b7583f1fa21
Khush Bakht Naveed <khush.naveed@dci-student.org>
2024-11-27 10:57:55 +0100

    complete

--- 14-spa-browser-fetch-saving-khushnaveed/scripts/index.js
+++ 14-spa-browser-fetch-saving-khushnaveed/scripts/index.js
@@ -1,5 +1,5 @@
-import View from './View.js';
-import Client from './Client.js';
+import View from "./View.js";
+import Client from "./Client.js";
 
 // All of your javascript should go here
 console.log("Hello from index.js");
@@ -7,28 +7,42 @@ console.log("Hello from index.js");
 const view = new View();
 const client = new Client();
 
-const input = document.querySelector('input');
-const saveBtn = document.querySelector('.btn-save');
-const resetBtn = document.querySelector('.btn-reset');
+const input = document.querySelector("input");
+const saveBtn = document.querySelector(".btn-save");
+const resetBtn = document.querySelector(".btn-reset");
 const movies = [];
 
-saveBtn.addEventListener('click', async () =>{
-    const movieName = input.value;
-    console.log(movieName);
-    if (!movieName) {
-        alert("Please enter a movie title.");
-        return;
-    }
-    
-    const movieData = await client.getMovieData(movieName);
-    console.log(movieData);
+saveBtn.addEventListener("click", async () => {
+  const movieName = input.value;
+  if (!movieName) {
+    alert("Please enter a movie title.");
+    return;
+  }
 
-    view.displayMovieOnPage(movieData);
+  // Check if the movie is already displayed
+  if (movies.includes(movieName.toLowerCase())) {
+    alert("This movie is already displayed on the page.");
+    input.value = "";
+    return;
+  }
+
+  const movieDataLocal = localStorage.getItem(movieName.toLowerCase());
+  if (movieDataLocal) {
+
+    view.displayMovieOnPage(JSON.parse(movieDataLocal));
+    movies.push(movieName.toLowerCase())
     input.value = "";
 
-    resetBtn.addEventListener('click', () =>{
-        view.removeDisplay();
-    })
-    
-    
-})
+  } else {
+    const movieData = await client.getMovieData(movieName);
+    console.log(movieData);
+    movies.push(movieName.toLowerCase());
+    view.displayMovieOnPage(movieData);
+    input.value = "";
+    localStorage.setItem(movieName.toLowerCase(), JSON.stringify(movieData));
+  }
+});
+resetBtn.addEventListener("click", () => {
+  view.removeDisplay();
+  localStorage.clear();
+});
